0b3af68d4733aa68efd20205340f6980d35938df
Kyrgyzbaev ADILET <98408321+ADIKOLORD@users.noreply.github.com>
2022-07-28 21:42:07 +0600

    Update lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -3,7 +3,7 @@
 dates_of_birth = {3, 10, 11, 13, 31, 21, 1, 10, 3, 5, 6, 6}
 dates_of_birth.discard(7)
 print(dates_of_birth)
-aaaffaaafaaaaafaaaafa
+aaaffaaafaaaaafaaaafaa
 # Problem 1
 farm_1 = {"dog", "cat", "mouse", "sheep"}
 farm_2 = {"cow", "horse", "donkey", "cat", "dog"}
